040a5d9399a852ce773d9bb64a29b82e3695c5d5
David <davidroussel171113@outlook.fr>
2024-10-16 22:28:43 +0200

    Ajout model firestations selon le fichier JSON

---
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Model/firestations.java
@@ -0,0 +1,29 @@
+package com.SafetyNet.SafetyNetAlerts.Model;
+
+public class firestations {
+
+    private String address;
+
+    private int station;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getStation() {
+        return station;
+    }
+
+    public void setStation(int station) {
+        this.station = station;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
