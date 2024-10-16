595d0590ce21ab9786feaba9ac4c2098f72a66bd
David <davidroussel171113@outlook.fr>
2024-10-16 23:34:20 +0200

    Ajout du BusinessService et utilisation pour appeler des méthodes des models

--- src/main/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplication.java
@@ -1,5 +1,10 @@
 package com.SafetyNet.SafetyNetAlerts;
 
+import com.SafetyNet.SafetyNetAlerts.Model.Firestations;
+import com.SafetyNet.SafetyNetAlerts.Model.MedicalRecords;
+import com.SafetyNet.SafetyNetAlerts.Model.Persons;
+import com.SafetyNet.SafetyNetAlerts.Service.BusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,13 +12,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SafetyNetAlertsApplication implements CommandLineRunner {
 
+	@Autowired
+	private BusinessService businessService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World !!!");
+		Persons bonjour = businessService.getPersons();
+		String bonjourSpring = bonjour.ditBonjour();
+		Firestations pompier = businessService.getFirestations();
+		String lesPompiers = pompier.lesPompier();
+		MedicalRecords medical = businessService.getMedicalRecords();
+		String registreMedical = medical.leRegistre();
+		System.out.println(bonjourSpring);
+		System.out.println(lesPompiers);
+		System.out.println(registreMedical);
 	}
 
 }
