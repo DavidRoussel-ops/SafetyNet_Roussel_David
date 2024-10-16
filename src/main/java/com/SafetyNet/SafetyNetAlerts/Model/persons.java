7c622981629f8210377a9b7bcb2d30c317b401ce
David <davidroussel171113@outlook.fr>
2024-10-16 22:27:27 +0200

    Ajout model persons selon le fichier JSON

---
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Model/persons.java
@@ -0,0 +1,79 @@
+package com.SafetyNet.SafetyNetAlerts.Model;
+
+public class persons {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String city;
+
+    private int zip;
+
+    private int phone;
+
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
