4a0e6a215dd3957fe717a03ceae91bc9e1d7b981
David <davidroussel171113@outlook.fr>
2024-10-19 16:11:10 +0200

    Première utilisation du beans BusinessService

--- src/main/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplication.java
@@ -1,13 +1,26 @@
 package com.SafetyNet.SafetyNetAlerts;
 
+import com.SafetyNet.SafetyNetAlerts.Model.Persons;
+import com.SafetyNet.SafetyNetAlerts.Service.BusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SafetyNetAlertsApplication {
+public class SafetyNetAlertsApplication implements CommandLineRunner {
+
+	@Autowired
+	BusinessService businessService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		Persons persons = businessService.getPersons();
+		persons.setFirstname("John");
+		System.out.println(persons.getFirstname());
 	}
 
 }
