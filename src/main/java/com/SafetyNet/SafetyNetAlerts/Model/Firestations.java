a5a0032ed8f2f287bda84197434a7467f7746968
David <davidroussel171113@outlook.fr>
2024-10-19 15:45:04 +0200

    Ajout model station de pompier

---
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Model/Firestations.java
@@ -0,0 +1,29 @@
+package com.SafetyNet.SafetyNetAlerts.Model;
+
+public class Firestations {
+
+    private String address;
+
+    private int station;
+
+    public int getStation() {
+        return station;
+    }
+
+    public void setStation(int station) {
+        this.station = station;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
