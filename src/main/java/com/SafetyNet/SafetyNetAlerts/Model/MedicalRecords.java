7e5b88d98132485a85da9898dbbedbda8145faa5
David <davidroussel171113@outlook.fr>
2024-10-19 15:45:41 +0200

    Ajout model dossier médical

---
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Model/MedicalRecords.java
@@ -0,0 +1,61 @@
+package com.SafetyNet.SafetyNetAlerts.Model;
+
+import java.util.Date;
+
+public class MedicalRecords {
+
+    private String firstname;
+
+    private String lastname;
+
+    private Date birthdate;
+
+    private String[] medications;
+
+    private String[] allergies;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String[] getMedications() {
+        return medications;
+    }
+
+    public void setMedications(String[] medications) {
+        this.medications = medications;
+    }
+
+    public String[] getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String[] allergies) {
+        this.allergies = allergies;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
