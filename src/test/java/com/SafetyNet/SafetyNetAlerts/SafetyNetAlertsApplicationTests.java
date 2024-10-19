44ad5d65dc38f4edfb42a869f19a36abc81c94b9
David <davidroussel171113@outlook.fr>
2024-10-19 16:17:34 +0200

    Ajout d'un premier test en Spring en utilisant le beans BusinessService

--- src/test/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplicationTests.java
+++ src/test/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplicationTests.java
@@ -1,13 +1,28 @@
 package com.SafetyNet.SafetyNetAlerts;
 
+import com.SafetyNet.SafetyNetAlerts.Model.Persons;
+import com.SafetyNet.SafetyNetAlerts.Service.BusinessService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SafetyNetAlertsApplicationTests {
 
+	@Autowired
+	BusinessService businessService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getPersonsTest() {
+		String expected = "John";
+		Persons persons = businessService.getPersons();
+		persons.setFirstname("John");
+		Assertions.assertEquals(expected, persons.getFirstname());
 	}
 
 }
