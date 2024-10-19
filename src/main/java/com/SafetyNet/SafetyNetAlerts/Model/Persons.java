1ecd96943f1e2346d40ddd575ce73c8a0bb150ea
David <davidroussel171113@outlook.fr>
2024-10-19 15:46:16 +0200

    Ajout model des personnes

---
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Model/Persons.java
@@ -0,0 +1,79 @@
+package com.SafetyNet.SafetyNetAlerts.Model;
+
+public class Persons {
+
+    private String firstname;
+
+    private String lastname;
+
+    private String address;
+
+    private String city;
+
+    private int zip;
+
+    private int phone;
+
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
