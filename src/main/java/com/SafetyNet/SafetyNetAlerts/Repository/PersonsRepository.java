a3da4c8eca36e3deabe0bcf71a9769d7f5ad32c7
David <davidroussel171113@outlook.fr>
2024-10-22 22:03:44 +0200

    Mise en place d'une instance qui extends CrudRepository pour utilisation method CRUD

---
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Repository/PersonsRepository.java
@@ -0,0 +1,9 @@
+package com.SafetyNet.SafetyNetAlerts.Repository;
+
+import com.SafetyNet.SafetyNetAlerts.Model.Persons;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonsRepository extends CrudRepository<Persons, Long> {
+
+
+}
