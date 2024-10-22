88938faf45e942f7bed6354d6bb9c2175b3aeef6
David <davidroussel171113@outlook.fr>
2024-10-22 22:01:29 +0200

    Modification class Persons pour enregistrement en BDD

--- src/main/java/com/SafetyNet/SafetyNetAlerts/Model/Persons.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Model/Persons.java
@@ -1,76 +1,28 @@
 package com.SafetyNet.SafetyNetAlerts.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
 public class Persons {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String firstname;
-
     private String lastname;
-
     private String address;
-
     private String city;
-
-    private int zip;
-
-    private int phone;
-
+    private String zip;
+    private String phone;
     private String email;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPhone() {
-        return phone;
-    }
-
-    public void setPhone(int phone) {
-        this.phone = phone;
-    }
-
-    public int getZip() {
-        return zip;
-    }
-
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
+    public Persons() {}
 
     @Override
     public String toString() {
