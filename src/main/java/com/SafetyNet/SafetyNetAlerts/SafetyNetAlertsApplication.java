346f0bc7bfc5a46f1a7a2a2ec0e8621c5a315ed8
David <davidroussel171113@outlook.fr>
2024-10-24 22:20:13 +0200

    Retrait du bean

--- src/main/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplication.java
@@ -26,24 +26,6 @@ public class SafetyNetAlertsApplication implements CommandLineRunner {
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner runner(PersonService personService) {
-		return args -> {
-			// Lire JSON et écrire en BDD
-			ObjectMapper mapper = new ObjectMapper();
-			mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
-			TypeReference<List<Persons>> typeReference = new TypeReference<List<Persons>>(){};
-			InputStream inputStream = TypeReference.class.getResourceAsStream("/data.json");
-			try {
-				List<Persons> persons = mapper.readValue(inputStream, typeReference);
-				personService.save(persons);
-				System.out.println("Persons enregistrer");
-			} catch (IOException ioException) {
-				System.out.println("Persons non enregistrer " + ioException.getMessage());
-			}
-		};
-	}
-
 	public void run(String... args) throws Exception {
 		System.out.println("L'application est lancer");
 	}
