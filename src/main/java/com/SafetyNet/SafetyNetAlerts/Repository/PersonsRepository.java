5289ed7106633c1fa87784037c1acc4605bcea40
David <davidroussel171113@outlook.fr>
2024-10-24 22:28:54 +0200

    Ajout annotation @Repository

--- src/main/java/com/SafetyNet/SafetyNetAlerts/Repository/PersonsRepository.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Repository/PersonsRepository.java
@@ -2,7 +2,9 @@ package com.SafetyNet.SafetyNetAlerts.Repository;
 
 import com.SafetyNet.SafetyNetAlerts.Model.Persons;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonsRepository extends CrudRepository<Persons, Long> {
 
 
