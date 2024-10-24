b0a1b8e6f64f14f23286ff8aaecc0665874fdddd
David <davidroussel171113@outlook.fr>
2024-10-24 22:31:51 +0200

    Modification de la class service pour Persons

--- src/main/java/com/SafetyNet/SafetyNetAlerts/Service/PersonService.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Service/PersonService.java
@@ -2,28 +2,32 @@ package com.SafetyNet.SafetyNetAlerts.Service;
 
 import com.SafetyNet.SafetyNetAlerts.Model.Persons;
 import com.SafetyNet.SafetyNetAlerts.Repository.PersonsRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Optional;
 
+@Data
 @Service
 public class PersonService {
 
+    @Autowired
     private final PersonsRepository personsRepository;
 
-    public PersonService(PersonsRepository personsRepository) {
-        this.personsRepository = personsRepository;
+    public Optional<Persons> getPerson(final Long id) {
+        return personsRepository.findById(id);
     }
 
-    public Iterable<Persons> list() {
+    public Iterable<Persons> getPersons() {
         return personsRepository.findAll();
     }
 
-    public Persons save(Persons persons) {
-        return personsRepository.save(persons);
+    public void deletePerson(final Long id) {
+        personsRepository.deleteById(id);
     }
 
-    public void save(List<Persons> persons) {
-        personsRepository.saveAll(persons);
+    public Persons savePerson(Persons persons) {
+        return personsRepository.save(persons);
     }
 }
