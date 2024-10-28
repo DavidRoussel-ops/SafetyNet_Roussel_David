ca9d708855e2fc9af4617650980f6f13d8884d72
David <davidroussel171113@outlook.fr>
2024-10-28 23:40:40 +0100

    Mise en place méthod pour lecture du JSON et écriture en BDD

--- src/main/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplication.java
@@ -1,7 +1,11 @@
 package com.SafetyNet.SafetyNetAlerts;
 
+import com.SafetyNet.SafetyNetAlerts.Model.Firestations;
+import com.SafetyNet.SafetyNetAlerts.Model.MedicalRecords;
 import com.SafetyNet.SafetyNetAlerts.Model.Persons;
 import com.SafetyNet.SafetyNetAlerts.Service.BusinessService;
+import com.SafetyNet.SafetyNetAlerts.Service.FirestationsService;
+import com.SafetyNet.SafetyNetAlerts.Service.MedicalRecordsService;
 import com.SafetyNet.SafetyNetAlerts.Service.PersonService;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,6 +26,12 @@ public class SafetyNetAlertsApplication implements CommandLineRunner {
 	@Autowired
 	PersonService personService;
 
+	@Autowired
+	FirestationsService firestationsService;
+
+	@Autowired
+	MedicalRecordsService medicalRecordsService;
+
     public static void main(String[] args) {
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
 	}
@@ -30,14 +40,14 @@ public class SafetyNetAlertsApplication implements CommandLineRunner {
 		System.out.println("L'application est lancer");
 		{
 			try {
-				Persons persons = new Persons();
 				ObjectMapper mapper = new ObjectMapper();
 				File file = new File("src/main/resources/data.json");
 				JsonNode jsonNode = mapper.readTree(file);
-				JsonNode locatedNode = jsonNode.path("persons");
-				//System.out.println("jsonNode : " + jsonNode);
-				//System.out.println("locatedNode : " + locatedNode);
-				for (JsonNode node : locatedNode) {
+				JsonNode PersonsNode = jsonNode.path("persons");
+				JsonNode FirestationsNode = jsonNode.path("firestations");
+				JsonNode MedicalRecordsNode = jsonNode.path("medicalrecords");
+				for (JsonNode node : PersonsNode) {
+					Persons persons = new Persons();
 					persons.setFirstname(node.path("firstName").asText());
 					persons.setLastname(node.path("lastName").asText());
 					persons.setAddress(node.path("address").asText());
@@ -45,8 +55,22 @@ public class SafetyNetAlertsApplication implements CommandLineRunner {
 					persons.setZip(node.path("zip").asText());
 					persons.setPhone(node.path("phone").asText());
 					persons.setEmail(node.path("email").asText());
-					personService.getPersons();
 					personService.savePerson(persons);
+				}
+				for (JsonNode node : FirestationsNode) {
+					Firestations firestations = new Firestations();
+					firestations.setAddress(node.path("address").asText());
+					firestations.setStation(node.path("station").asInt());
+					firestationsService.saveFirestation(firestations);
+				}
+				for (JsonNode node : MedicalRecordsNode) {
+					MedicalRecords medicalRecords = new MedicalRecords();
+					medicalRecords.setFirstname(node.path("firstName").asText());
+					medicalRecords.setLastname(node.path("lastName").asText());
+					medicalRecords.setBirthdate(node.path("birthdate").asText());
+					medicalRecords.setMedications(node.path("medications").asText());
+					medicalRecords.setAllergies(node.path("allergies").asText());
+					medicalRecordsService.saveMedicalRecord(medicalRecords);
 				}
 			} catch (IOException e) {
 				throw new RuntimeException(e);
