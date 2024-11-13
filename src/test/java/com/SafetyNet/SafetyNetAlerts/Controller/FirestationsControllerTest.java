a225985c7afc3a6408c70727e2bf4a12ac73155b
David <davidroussel171113@outlook.fr>
2024-11-14 00:22:51 +0100

    Ajout de test pour vérifié la récupération d'un éléments et ajout d'un auutre

--- src/test/java/com/SafetyNet/SafetyNetAlerts/Controller/FirestationsControllerTest.java
+++ src/test/java/com/SafetyNet/SafetyNetAlerts/Controller/FirestationsControllerTest.java
@@ -5,6 +5,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.SafetyNet.SafetyNetAlerts.Model.Firestations;
+import com.SafetyNet.SafetyNetAlerts.Model.Persons;
+import com.SafetyNet.SafetyNetAlerts.Service.FirestationsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,10 +22,32 @@ public class FirestationsControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private FirestationsService service;
+
     @Test
     public void testGetFirestations() throws Exception {
         mockMvc.perform(get("/firestation"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetOnePerson() throws Exception {
+        mockMvc.perform(get("/firestation/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("station", is("3")));
+    }
+
+    @Test
+    public void testAddOnePerson() throws Exception {
+        Firestations firestations = new Firestations();
+        firestations.setStation("2");
+        firestations.setAddress("1254 street of street");
+        firestations.setId(14L);
+        service.saveFirestation(firestations);
+        mockMvc.perform(get("/firestation/14"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("address", is("1254 street of street")));
     }
 
 }
