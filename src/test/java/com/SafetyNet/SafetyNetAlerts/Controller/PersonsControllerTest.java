3ce04ff8492fc3b79d432e2177b07d4af423822f
David <davidroussel171113@outlook.fr>
2024-11-26 23:44:17 +0100

    Ajout des tests testGetMail, testGetMailNotFound et base du test testGetPersonsByLastname

--- src/test/java/com/SafetyNet/SafetyNetAlerts/Controller/PersonsControllerTest.java
+++ src/test/java/com/SafetyNet/SafetyNetAlerts/Controller/PersonsControllerTest.java
@@ -31,6 +31,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -77,6 +78,96 @@ public class PersonsControllerTest {
         );
         when(service.getPersons()).thenReturn(persons);
         mockMvc.perform(get("/person"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void testGetMail() throws Exception {
+        String city = "Culver";
+        Persons persons1 = new Persons();
+        persons1.setId(1L);
+        persons1.setFirstname("Paul");
+        persons1.setLastname("Boyd");
+        persons1.setAddress("1509 Culver St");
+        persons1.setCity("Culver");
+        persons1.setZip("97451");
+        persons1.setPhone("841-874-6512");
+        persons1.setEmail("jaboy@email.com");
+        Persons persons2 = new Persons();
+        persons2.setId(2L);
+        persons2.setFirstname("Paul");
+        persons2.setLastname("Boyd");
+        persons2.setAddress("1509 Culver St");
+        persons2.setCity("Culver");
+        persons2.setZip("97451");
+        persons2.setPhone("841-874-6512");
+        persons2.setEmail("paul@email.com");
+        Iterable<Persons> persons = new ArrayList<>(
+                Arrays.asList(persons1, persons2)
+        );
+        when(service.getPersons()).thenReturn(persons);
+        mockMvc.perform(get("/communityEmail?city={city}", city))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void testGetMailNNotFound() throws Exception {
+        String city = "Paris";
+        Persons persons1 = new Persons();
+        persons1.setId(1L);
+        persons1.setFirstname("Paul");
+        persons1.setLastname("Boyd");
+        persons1.setAddress("1509 Culver St");
+        persons1.setCity("Culver");
+        persons1.setZip("97451");
+        persons1.setPhone("841-874-6512");
+        persons1.setEmail("jaboy@email.com");
+        Persons persons2 = new Persons();
+        persons2.setId(2L);
+        persons2.setFirstname("Paul");
+        persons2.setLastname("Boyd");
+        persons2.setAddress("1509 Culver St");
+        persons2.setCity("Culver");
+        persons2.setZip("97451");
+        persons2.setPhone("841-874-6512");
+        persons2.setEmail("paul@email.com");
+        Iterable<Persons> persons = new ArrayList<>(
+                Arrays.asList(persons1, persons2)
+        );
+        when(service.getPersons()).thenReturn(persons);
+        mockMvc.perform(get("/communityEmail?city={city}", city))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @Test
+    public void testGetPersonsByLastname() throws Exception {
+        String lastname = "Boyd";
+        Persons persons1 = new Persons();
+        persons1.setId(1L);
+        persons1.setFirstname("Paul");
+        persons1.setLastname("Boyd");
+        persons1.setAddress("1509 Culver St");
+        persons1.setCity("Culver");
+        persons1.setZip("97451");
+        persons1.setPhone("841-874-6512");
+        persons1.setEmail("jaboy@email.com");
+        /*Persons persons2 = new Persons();
+        persons2.setId(2L);
+        persons2.setFirstname("Paul");
+        persons2.setLastname("Boyd");
+        persons2.setAddress("1509 Culver St");
+        persons2.setCity("Culver");
+        persons2.setZip("97451");
+        persons2.setPhone("841-874-6512");
+        persons2.setEmail("paul@email.com");*/
+        Iterable<Persons> persons = new ArrayList<>(
+                Arrays.asList(persons1)
+        );
+        when(service.getPersons()).thenReturn(persons);
+        mockMvc.perform(get("/?personInfolastName={lastname}", lastname))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
