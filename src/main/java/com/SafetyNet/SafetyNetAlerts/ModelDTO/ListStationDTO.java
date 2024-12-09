3528263c26042bcb301c4834e29e3f49aa013d4d
David <davidroussel171113@outlook.fr>
2024-12-09 23:39:17 +0100

    Ajout d'une classe DTO pour la méthode getHomeByStation()

---
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/ModelDTO/ListStationDTO.java
@@ -0,0 +1,17 @@
+package com.SafetyNet.SafetyNetAlerts.ModelDTO;
+
+import lombok.Data;
+
+@Data
+public class ListStationDTO {
+
+    private String lastname;
+
+    private String phone;
+
+    private int age;
+
+    private String medications;
+
+    private String allergies;
+}
