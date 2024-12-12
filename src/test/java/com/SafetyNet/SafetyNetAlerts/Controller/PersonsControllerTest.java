cd76f0faea244b6998090a6df6f5c314459e3f89
David <davidroussel171113@outlook.fr>
2024-12-12 17:24:32 +0100

    Finalisation test getHomeByStation()

--- src/test/java/com/SafetyNet/SafetyNetAlerts/Controller/PersonsControllerTest.java
+++ src/test/java/com/SafetyNet/SafetyNetAlerts/Controller/PersonsControllerTest.java
@@ -8,8 +8,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.SafetyNet.SafetyNetAlerts.Model.Firestations;
 import com.SafetyNet.SafetyNetAlerts.Model.MedicalRecords;
 import com.SafetyNet.SafetyNetAlerts.Model.Persons;
+import com.SafetyNet.SafetyNetAlerts.Repository.FirestationsRepository;
 import com.SafetyNet.SafetyNetAlerts.Repository.PersonsRepository;
 import com.SafetyNet.SafetyNetAlerts.Service.BusinessService;
 import com.SafetyNet.SafetyNetAlerts.Service.FirestationsService;
@@ -114,7 +116,7 @@ public class PersonsControllerTest {
         persons3.setPhone("841-874-6512");
         persons3.setEmail("michel@email.com");
         Iterable<Persons> persons = new ArrayList<>(
-                Arrays.asList(persons1, persons2, persons3  )
+                Arrays.asList(persons1, persons2, persons3)
         );
         when(service.getPersons()).thenReturn(persons);
         mockMvc.perform(get("/communityEmail?city={city}", city))
@@ -151,6 +153,87 @@ public class PersonsControllerTest {
                 .andExpect(status().isNotFound())
                 .andDo(print());
     }
+
+    @Test
+    public void getHomeByStation() throws Exception {
+        String stations = "3";
+        Firestations firestations1 = new Firestations();
+        firestations1.setId(1L);
+        firestations1.setAddress("15 rue de la gloire");
+        firestations1.setStation("2");
+        Firestations firestations2 = new Firestations();
+        firestations2.setId(2L);
+        firestations2.setAddress("10 rue de la gloire");
+        firestations2.setStation("1");
+        Firestations firestations3 = new Firestations();
+        firestations3.setId(3L);
+        firestations3.setAddress("12 rue de la gloire");
+        firestations3.setStation("3");
+        Persons persons1 = new Persons();
+        persons1.setId(1L);
+        persons1.setFirstname("Michel");
+        persons1.setLastname("Boyd");
+        persons1.setAddress("15 rue de la gloire");
+        persons1.setCity("Culver");
+        persons1.setZip("97451");
+        persons1.setPhone("841-874-6512");
+        persons1.setEmail("michel@email.com");
+        Persons persons2 = new Persons();
+        persons2.setId(2L);
+        persons2.setFirstname("Paul");
+        persons2.setLastname("Boyd");
+        persons2.setAddress("10 rue de la gloire");
+        persons2.setCity("Culver");
+        persons2.setZip("97465");
+        persons2.setPhone("841-874-3254");
+        persons2.setEmail("paul@email.com");
+        Persons persons3 = new Persons();
+        persons3.setId(3L);
+        persons3.setFirstname("Jacques");
+        persons3.setLastname("Boyd");
+        persons3.setAddress("10 rue de la gloire");
+        persons3.setCity("Culver");
+        persons3.setZip("97451");
+        persons3.setPhone("841-874-4596");
+        persons3.setEmail("jacques@email.com");
+        MedicalRecords medicalRecords1 = new MedicalRecords();
+        medicalRecords1.setId(1L);
+        medicalRecords1.setFirstname("Michel");
+        medicalRecords1.setLastname("Boyd");
+        medicalRecords1.setBirthdate("01/01/1965");
+        medicalRecords1.setMedications("Paracetamol : 1000mg");
+        medicalRecords1.setAllergies("Aspirine");
+        MedicalRecords medicalRecords2 = new MedicalRecords();
+        medicalRecords2.setId(2L);
+        medicalRecords2.setFirstname("Paul");
+        medicalRecords2.setLastname("Boyd");
+        medicalRecords2.setBirthdate("01/01/1985");
+        medicalRecords2.setMedications("Paracetamol : 500mg");
+        medicalRecords2.setAllergies("Codéine");
+        MedicalRecords medicalRecords3 = new MedicalRecords();
+        medicalRecords3.setId(3L);
+        medicalRecords3.setFirstname("Jacques");
+        medicalRecords3.setLastname("Boyd");
+        medicalRecords3.setBirthdate("01/01/1999");
+        medicalRecords3.setMedications("Codéine : 1500mg");
+        medicalRecords3.setAllergies("Paracetamol");
+        Iterable<Persons> persons = new ArrayList<>(
+                Arrays.asList(persons1, persons2, persons3)
+        );
+        Iterable<MedicalRecords> medicalRecords = new ArrayList<>(
+                Arrays.asList(medicalRecords1, medicalRecords2, medicalRecords3)
+        );
+        Iterable<Firestations> firestations = new ArrayList<>(
+                Arrays.asList(firestations1, firestations2, firestations3)
+        );
+        when(service.getPersons()).thenReturn(persons);
+        when(medicalRecordsService.getMedicalRecords()).thenReturn(medicalRecords);
+        when(firestationsService.getFirestations()).thenReturn(firestations);
+        mockMvc.perform(get("/flood/stations?stations={stations}", stations))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
 
     @Test
     public void testGetPersonsByLastnameFindOne() throws Exception {
