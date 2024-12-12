166f80db31e0ac4d64e320d199c69bd3f727e12d
David <davidroussel171113@outlook.fr>
2024-12-12 22:56:39 +0100

    Ajout d'un model DTO pour la méthode getAddress()

---
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/ModelDTO/FireAddressDTO.java
@@ -0,0 +1,19 @@
+package com.SafetyNet.SafetyNetAlerts.ModelDTO;
+
+import lombok.Data;
+
+@Data
+public class FireAddressDTO {
+
+    private String lastname;
+
+    private String phone;
+
+    private int age;
+
+    private String medications;
+
+    private String allergies;
+
+    private String stations;
+}
