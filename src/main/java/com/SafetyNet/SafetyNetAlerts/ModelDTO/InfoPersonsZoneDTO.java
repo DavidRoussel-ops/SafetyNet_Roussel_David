e25ae0752821aeeb712a326b0710ea2f060e20b0
David <davidroussel171113@outlook.fr>
2024-12-14 22:07:42 +0100

    Ajout d'une class pour la méthode getPersonsByZone()

---
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/ModelDTO/InfoPersonsZoneDTO.java
@@ -0,0 +1,16 @@
+package com.SafetyNet.SafetyNetAlerts.ModelDTO;
+
+import lombok.Data;
+
+@Data
+public class InfoPersonsZoneDTO {
+
+    private String firstname;
+
+    private String lastname;
+
+    private String address;
+
+    private String phone;
+
+}
