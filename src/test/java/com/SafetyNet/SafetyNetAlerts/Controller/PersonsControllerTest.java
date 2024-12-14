518701bf188105c49fbfd90da305418591fae49c
David <davidroussel171113@outlook.fr>
2024-12-14 14:03:48 +0100

    Ajout du test testGetChildAlert()

--- src/test/java/com/SafetyNet/SafetyNetAlerts/Controller/PersonsControllerTest.java
+++ src/test/java/com/SafetyNet/SafetyNetAlerts/Controller/PersonsControllerTest.java
@@ -210,6 +210,70 @@ public class PersonsControllerTest {
     }
 
     @Test
+    public void testGetChildAlert() throws Exception {
+        String address = "10 rue de la gloire";
+        Persons persons1 = new Persons();
+        persons1.setId(1L);
+        persons1.setFirstname("Michel");
+        persons1.setLastname("Boyd");
+        persons1.setAddress("15 rue de la gloire");
+        persons1.setCity("Culver");
+        persons1.setZip("97451");
+        persons1.setPhone("841-874-6512");
+        persons1.setEmail("michel@email.com");
+        Persons persons2 = new Persons();
+        persons2.setId(2L);
+        persons2.setFirstname("Paul");
+        persons2.setLastname("Boyd");
+        persons2.setAddress("10 rue de la gloire");
+        persons2.setCity("Culver");
+        persons2.setZip("97465");
+        persons2.setPhone("841-874-3254");
+        persons2.setEmail("paul@email.com");
+        Persons persons3 = new Persons();
+        persons3.setId(3L);
+        persons3.setFirstname("Jacques");
+        persons3.setLastname("Boyd");
+        persons3.setAddress("10 rue de la gloire");
+        persons3.setCity("Culver");
+        persons3.setZip("97451");
+        persons3.setPhone("841-874-4596");
+        persons3.setEmail("jacques@email.com");
+        MedicalRecords medicalRecords1 = new MedicalRecords();
+        medicalRecords1.setId(1L);
+        medicalRecords1.setFirstname("Michel");
+        medicalRecords1.setLastname("Boyd");
+        medicalRecords1.setBirthdate("01/01/1965");
+        medicalRecords1.setMedications("Paracetamol : 1000mg");
+        medicalRecords1.setAllergies("Aspirine");
+        MedicalRecords medicalRecords2 = new MedicalRecords();
+        medicalRecords2.setId(2L);
+        medicalRecords2.setFirstname("Paul");
+        medicalRecords2.setLastname("Boyd");
+        medicalRecords2.setBirthdate("01/01/1985");
+        medicalRecords2.setMedications("Paracetamol : 500mg");
+        medicalRecords2.setAllergies("Codéine");
+        MedicalRecords medicalRecords3 = new MedicalRecords();
+        medicalRecords3.setId(3L);
+        medicalRecords3.setFirstname("Jacques");
+        medicalRecords3.setLastname("Boyd");
+        medicalRecords3.setBirthdate("01/01/2014");
+        medicalRecords3.setMedications("Codéine : 1500mg");
+        medicalRecords3.setAllergies("Paracetamol");
+        Iterable<Persons> persons = new ArrayList<>(
+                Arrays.asList(persons1, persons2, persons3)
+        );
+        Iterable<MedicalRecords> medicalRecords = new ArrayList<>(
+                Arrays.asList(medicalRecords1, medicalRecords2, medicalRecords3)
+        );
+        when(service.getPersons()).thenReturn(persons);
+        when(medicalRecordsService.getMedicalRecords()).thenReturn(medicalRecords);
+        mockMvc.perform(get("/childAlert?address={address}", address))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
     public void testGetAddress() throws Exception {
         String address = "15 rue de la gloire";
         Firestations firestations1 = new Firestations();
