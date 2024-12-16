ba446698edbd5b3ea8e7d8ac364fd2ff5b9744f5
David <davidroussel171113@outlook.fr>
2024-12-16 21:50:29 +0100

    Clean code

--- src/test/java/com/SafetyNet/SafetyNetAlerts/Controller/MedicalRecordsControllerTest.java
+++ src/test/java/com/SafetyNet/SafetyNetAlerts/Controller/MedicalRecordsControllerTest.java
@@ -1,6 +1,5 @@
 package com.SafetyNet.SafetyNetAlerts.Controller;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
