558bf28e7187c591913406f9f77ee113406b5d6b
David <davidroussel171113@outlook.fr>
2025-01-14 22:55:52 +0100

    Ajout de la méthode putPerson()

--- src/main/java/com/SafetyNet/SafetyNetAlerts/Service/PersonService.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Service/PersonService.java
@@ -4,6 +4,8 @@ import com.SafetyNet.SafetyNetAlerts.Model.Persons;
 import com.SafetyNet.SafetyNetAlerts.Repository.PersonsRepository;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -43,5 +45,44 @@ public class PersonService {
      */
     public Persons savePerson(Persons persons) {
         return personsRepository.save(persons);
+    }
+
+    public Persons putPerson(final Long id, final Persons persons) {
+        Optional<Persons> personsOptional = getPerson(id);
+        if (personsOptional.isPresent()) {
+            Persons currentPersons = personsOptional.get();
+
+            String firstName = persons.getFirstname();
+            if (firstName != null) {
+                currentPersons.setFirstname(firstName);
+            }
+            String lastName = persons.getLastname();
+            if (lastName != null) {
+                currentPersons.setLastname(lastName);
+            }
+            String address = persons.getAddress();
+            if (address != null) {
+                currentPersons.setAddress(address);
+            }
+            String city = persons.getCity();
+            if (city != null) {
+                currentPersons.setCity(city);
+            }
+            String zip = persons.getZip();
+            if (zip != null) {
+                currentPersons.setZip(zip);
+            }
+            String phone = persons.getPhone();
+            if (phone != null) {
+                currentPersons.setPhone(phone);
+            }
+            String email = persons.getEmail();
+            if (email != null) {
+                currentPersons.setEmail(email);
+            }
+            return savePerson(currentPersons);
+        } else {
+            return null;
+        }
     }
 }
