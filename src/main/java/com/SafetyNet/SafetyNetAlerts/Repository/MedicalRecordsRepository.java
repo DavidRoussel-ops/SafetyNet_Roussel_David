34fd5c984ef3aa06289be3278ce8847ae1313469
David <davidroussel171113@outlook.fr>
2025-01-15 23:23:33 +0100

    Modification de l'interface pour la prise ne chage du fichier JSON

--- src/main/java/com/SafetyNet/SafetyNetAlerts/Repository/MedicalRecordsRepository.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Repository/MedicalRecordsRepository.java
@@ -1,10 +1,37 @@
 package com.SafetyNet.SafetyNetAlerts.Repository;
 
 import com.SafetyNet.SafetyNetAlerts.Model.MedicalRecords;
+import com.SafetyNet.SafetyNetAlerts.Service.MedicalRecordsService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.io.File;
+import java.io.IOException;
 
 @Repository
 public interface MedicalRecordsRepository extends CrudRepository<MedicalRecords, Long> {
 
+    static MedicalRecords medialRecordsJSON() {
+        try {
+            MedicalRecordsService medicalRecordsService = new MedicalRecordsService();
+            ObjectMapper mapper = new ObjectMapper();
+            File file = new File("src/main/resources/data.json");
+            JsonNode jsonNode = mapper.readTree(file);
+            JsonNode MedicalRecordsNode = jsonNode.path("medicalrecords");
+            for (JsonNode node : MedicalRecordsNode) {
+                MedicalRecords medicalRecords = new MedicalRecords();
+                medicalRecords.setFirstname(node.path("firstName").asText());
+                medicalRecords.setLastname(node.path("lastName").asText());
+                medicalRecords.setBirthdate(node.path("birthdate").asText());
+                medicalRecords.setMedications(String.valueOf(node.findValue("medications")));
+                medicalRecords.setAllergies(String.valueOf(node.findValue("allergies")));
+                medicalRecordsService.saveMedicalRecord(medicalRecords);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 }
