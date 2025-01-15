94a5ea9ef226bb8bba6aca4d646e386bfa8bb377
David <davidroussel171113@outlook.fr>
2025-01-15 23:26:56 +0100

    Remise en place de la class

--- src/main/java/com/SafetyNet/SafetyNetAlerts/Service/FirestationsService.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Service/FirestationsService.java
@@ -2,15 +2,11 @@ package com.SafetyNet.SafetyNetAlerts.Service;
 
 import com.SafetyNet.SafetyNetAlerts.Model.Firestations;
 import com.SafetyNet.SafetyNetAlerts.Repository.FirestationsRepository;
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Data
 @Service
 public class FirestationsService {
 
