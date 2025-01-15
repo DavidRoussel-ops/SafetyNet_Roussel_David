14156290c55e6d94db278cf9bd3817ba6ef71eb9
David <davidroussel171113@outlook.fr>
2025-01-15 23:18:48 +0100

    Modification du model et ajout de getter/setter

--- src/main/java/com/SafetyNet/SafetyNetAlerts/Model/Firestations.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Model/Firestations.java
@@ -3,19 +3,30 @@ package com.SafetyNet.SafetyNetAlerts.Model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Data
 @Entity
-@Table(name = "firestations")
 public class Firestations {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "address")
     private String address;
 
-    @Column(name = "station")
     private String station;
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public void setStation(String station) {
+        this.station = station;
+    }
 }
