8a566104cd17ba58adac49e1fdd4df50312e1c47
David <davidroussel171113@outlook.fr>
2025-01-17 00:26:26 +0100

    Ajout getter/setter pour l'id

--- src/main/java/com/SafetyNet/SafetyNetAlerts/Model/Firestations.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Model/Firestations.java
@@ -14,6 +14,14 @@ public class Firestations {
 
     private String station;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getAddress() {
         return address;
     }
