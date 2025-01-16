559ebc354c0e8ba6bdea8c09bf7337af39d295f6
David <davidroussel171113@outlook.fr>
2025-01-17 00:27:19 +0100

    Ajout de méthode au repository

--- src/main/java/com/SafetyNet/SafetyNetAlerts/Repository/FirestationsRepository.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Repository/FirestationsRepository.java
@@ -9,27 +9,65 @@ import org.springframework.stereotype.Repository;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
 
 @Repository
 public interface FirestationsRepository extends CrudRepository<Firestations, Long> {
 
-    static Firestations firestationJSON() {
+    default ArrayList<Firestations> findAllFirestations() {
         try {
-            FirestationsService firestationsService = new FirestationsService();
+            Long id = 1L;
+            ArrayList<Firestations> firestationsArrayList = new ArrayList<>();
             ObjectMapper mapper = new ObjectMapper();
             File file = new File("src/main/resources/data.json");
             JsonNode jsonNode = mapper.readTree(file);
             JsonNode FirestationsNode = jsonNode.path("firestations");
             for (JsonNode node : FirestationsNode) {
                 Firestations firestations = new Firestations();
+                firestations.setId(id);
                 firestations.setAddress(node.path("address").asText());
                 firestations.setStation(node.path("station").asText());
-                firestationsService.saveFirestation(firestations);
+                firestationsArrayList.add(firestations);
+                id ++;
             }
+            return firestationsArrayList;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    default void saveFirestation(Firestations firestations) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("src/main/resources/data.json");
+        JsonNode jsonNode = mapper.readTree(file);
+        JsonNode FirestationsNode = jsonNode.path("firestations");
+        ArrayList<Firestations> firestationsArrayList = findAllFirestations();
+        Firestations lastFirestation = firestationsArrayList.get(firestationsArrayList.size() - 1);
+        Long lastId = lastFirestation.getId();
+        Long newId = lastId + 1;
+        Firestations firestationToSave = new Firestations();
+        firestationToSave.setId(newId);
+        firestationToSave.setStation(firestations.getStation());
+        firestationToSave.setAddress(firestations.getAddress());
+        firestationsArrayList.add(firestationToSave);
+        System.out.println(firestationToSave);
+        /*for (JsonNode node : FirestationsNode) {
+
+        }*/
+        mapper.writeValue(file, firestationsArrayList);
+    }
+
+    default Firestations findFirestationById(final Long id) {
+        ArrayList<Firestations> firestations = findAllFirestations();
+        for (Firestations firestation : firestations) {
+            if (Objects.equals(id, firestation.getId())) {
+                return firestation;
+            }
+        }
         return null;
     }
+
+
 
 }
