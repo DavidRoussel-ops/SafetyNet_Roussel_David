72902cd496462ca4fe31341514c2f90e2348a390
David <davidroussel171113@outlook.fr>
2025-01-23 00:45:03 +0100

    Modification suite au passage en json

--- src/main/java/com/SafetyNet/SafetyNetAlerts/Service/BusinessService.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Service/BusinessService.java
@@ -3,18 +3,28 @@ package com.SafetyNet.SafetyNetAlerts.Service;
 import com.SafetyNet.SafetyNetAlerts.Model.Firestations;
 import com.SafetyNet.SafetyNetAlerts.Model.MedicalRecords;
 import com.SafetyNet.SafetyNetAlerts.Model.Persons;
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonPointer;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class BusinessService {
 
-    /*@Autowired
+    @Autowired
     PersonService personService;
 
     @Autowired
@@ -28,37 +38,38 @@ public class BusinessService {
             ObjectMapper mapper = new ObjectMapper();
             File file = new File("src/main/resources/data.json");
             JsonNode jsonNode = mapper.readTree(file);
-            JsonNode PersonsNode = jsonNode.path("persons");
+            mapper.writerWithDefaultPrettyPrinter().writeValue(file, jsonNode);
+            /*JsonNode PersonsNode = jsonNode.path("persons");
             JsonNode FirestationsNode = jsonNode.path("firestations");
             JsonNode MedicalRecordsNode = jsonNode.path("medicalrecords");
             for (JsonNode node : PersonsNode) {
                 Persons persons = new Persons();
-                persons.setFirstname(node.path("firstName").asText());
-                persons.setLastname(node.path("lastName").asText());
+                persons.setFirstName(node.path("firstName").asText());
+                persons.setLastName(node.path("lastName").asText());
                 persons.setAddress(node.path("address").asText());
                 persons.setCity(node.path("city").asText());
                 persons.setZip(node.path("zip").asText());
                 persons.setPhone(node.path("phone").asText());
                 persons.setEmail(node.path("email").asText());
-                personService.savePerson(persons);
+                mapper.writerWithDefaultPrettyPrinter().writeValue(file, PersonsNode);
             }
             for (JsonNode node : FirestationsNode) {
                 Firestations firestations = new Firestations();
                 firestations.setAddress(node.path("address").asText());
                 firestations.setStation(node.path("station").asText());
-                firestationsService.saveFirestation(firestations);
+                mapper.writerWithDefaultPrettyPrinter().writeValue(file, FirestationsNode);
             }
             for (JsonNode node : MedicalRecordsNode) {
                 MedicalRecords medicalRecords = new MedicalRecords();
-                medicalRecords.setFirstname(node.path("firstName").asText());
-                medicalRecords.setLastname(node.path("lastName").asText());
+                medicalRecords.setFirstName(node.path("firstName").asText());
+                medicalRecords.setLastName(node.path("lastName").asText());
                 medicalRecords.setBirthdate(node.path("birthdate").asText());
                 medicalRecords.setMedications(String.valueOf(node.findValue("medications")));
                 medicalRecords.setAllergies(String.valueOf(node.findValue("allergies")));
-                medicalRecordsService.saveMedicalRecord(medicalRecords);
-            }
+                mapper.writerWithDefaultPrettyPrinter().writeValue(file, MedicalRecordsNode);
+            }*/
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }*/
+    }
 }
