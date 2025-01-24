9ba3b4e19db27ec418b2dbacaec98b667f584af5
David <davidroussel171113@outlook.fr>
2025-01-24 17:12:44 +0100

    Modification des test pour accord nouveau code

--- src/test/java/com/SafetyNet/SafetyNetAlerts/Controller/MedicalRecordsControllerTest.java
+++ src/test/java/com/SafetyNet/SafetyNetAlerts/Controller/MedicalRecordsControllerTest.java
@@ -1,10 +1,8 @@
 package com.SafetyNet.SafetyNetAlerts.Controller;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.SafetyNet.SafetyNetAlerts.Model.MedicalRecords;
@@ -22,16 +20,12 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Optional;
 
 @WebMvcTest
 public class MedicalRecordsControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private BusinessService businessService;
 
     @MockBean
     private MedicalRecordsService service;
@@ -44,28 +38,30 @@ public class MedicalRecordsControllerTest {
 
     @Test
     public void testGetMedicalRecords() throws Exception {
+        String[] arrayMedications = new String[]{"Dolliprane : 500mg"};
+        String[] arrayAllergies = new String[]{"aspirine"};
         MedicalRecords medicalRecords1 = new MedicalRecords();
         medicalRecords1.setId(1L);
-        medicalRecords1.setFirstname("Pierre");
-        medicalRecords1.setLastname("Dupont");
+        medicalRecords1.setFirstName("Pierre");
+        medicalRecords1.setLastName("Dupont");
         medicalRecords1.setBirthdate("01/01/1981");
-        medicalRecords1.setMedications("Dolliprane : 500mg");
-        medicalRecords1.setAllergies("aspirine");
+        medicalRecords1.setMedications(arrayMedications);
+        medicalRecords1.setAllergies(arrayAllergies);
         MedicalRecords medicalRecords2 = new MedicalRecords();
         medicalRecords2.setId(2L);
-        medicalRecords2.setFirstname("Paul");
-        medicalRecords2.setLastname("Dupont");
+        medicalRecords2.setFirstName("Paul");
+        medicalRecords2.setLastName("Dupont");
         medicalRecords2.setBirthdate("01/01/1981");
-        medicalRecords2.setMedications("Dolliprane : 500mg");
-        medicalRecords2.setAllergies("aspirine");
+        medicalRecords2.setMedications(arrayMedications);
+        medicalRecords2.setAllergies(arrayAllergies);
         MedicalRecords medicalRecords3 = new MedicalRecords();
         medicalRecords3.setId(3L);
-        medicalRecords3.setFirstname("Jacques");
-        medicalRecords3.setLastname("Dupont");
+        medicalRecords3.setFirstName("Jacques");
+        medicalRecords3.setLastName("Dupont");
         medicalRecords3.setBirthdate("01/01/1981");
-        medicalRecords3.setMedications("Dolliprane : 500mg");
-        medicalRecords3.setAllergies("aspirine");
-        Iterable<MedicalRecords> medicalRecords = new ArrayList<>(
+        medicalRecords3.setMedications(arrayMedications);
+        medicalRecords3.setAllergies(arrayAllergies);
+        ArrayList<MedicalRecords> medicalRecords = new ArrayList<>(
                 Arrays.asList(medicalRecords1, medicalRecords2, medicalRecords3)
         );
         when(service.getMedicalRecords()).thenReturn(medicalRecords);
@@ -76,15 +72,17 @@ public class MedicalRecordsControllerTest {
 
     @Test
     public void testGetOneMedicalRecords() throws Exception {
+        String[] arrayMedications = new String[]{"Dolliprane : 500mg"};
+        String[] arrayAllergies = new String[]{"aspirine"};
         Long id = 1L;
         MedicalRecords medicalRecords = new MedicalRecords();
         medicalRecords.setId(id);
-        medicalRecords.setFirstname("Pierre");
-        medicalRecords.setLastname("Dupont");
+        medicalRecords.setFirstName("Pierre");
+        medicalRecords.setLastName("Dupont");
         medicalRecords.setBirthdate("01/01/1981");
-        medicalRecords.setMedications("Dolliprane : 500mg");
-        medicalRecords.setAllergies("aspirine");
-        when(service.getMedicalRecord(id)).thenReturn(Optional.of(medicalRecords));
+        medicalRecords.setMedications(arrayMedications);
+        medicalRecords.setAllergies(arrayAllergies);
+        when(service.getMedicalRecord(id)).thenReturn(medicalRecords);
         mockMvc.perform(get("/medicalRecord/{id}", id))
                 .andExpect(status().isOk())
                 .andDo(print());
@@ -92,8 +90,7 @@ public class MedicalRecordsControllerTest {
 
     @Test
     public void testGetOneMedicalRecordsNotFound() throws Exception {
-        Long id = 1L;
-        when(service.getMedicalRecord(id)).thenReturn(Optional.empty());
+        Long id = null;
         mockMvc.perform(get("/medicalRecord/{id}", id))
                 .andExpect(status().isNotFound())
                 .andDo(print());
@@ -101,13 +98,15 @@ public class MedicalRecordsControllerTest {
 
     @Test
     public void testAddOneMedicalRecords() throws Exception {
+        String[] arrayMedications = new String[]{"Dolliprane : 500mg"};
+        String[] arrayAllergies = new String[]{"aspirine"};
         MedicalRecords medicalRecords = new MedicalRecords();
         medicalRecords.setId(null);
-        medicalRecords.setFirstname("Pierre");
-        medicalRecords.setLastname("Dupont");
+        medicalRecords.setFirstName("Pierre");
+        medicalRecords.setLastName("Dupont");
         medicalRecords.setBirthdate("01/01/1981");
-        medicalRecords.setMedications("Dolliprane : 500mg");
-        medicalRecords.setAllergies("aspirine");
+        medicalRecords.setMedications(arrayMedications);
+        medicalRecords.setAllergies(arrayAllergies);
         mockMvc.perform(post("/medicalRecord", medicalRecords)
                         .content(asJsonString(medicalRecords))
                         .contentType(MediaType.APPLICATION_JSON)
@@ -118,23 +117,25 @@ public class MedicalRecordsControllerTest {
 
     @Test
     public void testPutOneMedicalRecords() throws Exception {
+        String[] arrayMedications = new String[]{"Dolliprane : 500mg"};
+        String[] newArrayMedications = new String[]{"Dolliprane : 1000mg"};
+        String[] arrayAllergies = new String[]{"aspirine"};
         Long id = 1L;
         MedicalRecords medicalRecordsInitial = new MedicalRecords();
         medicalRecordsInitial.setId(id);
-        medicalRecordsInitial.setFirstname("Pierre");
-        medicalRecordsInitial.setLastname("Dupont");
+        medicalRecordsInitial.setFirstName("Pierre");
+        medicalRecordsInitial.setLastName("Dupont");
         medicalRecordsInitial.setBirthdate("01/01/1981");
-        medicalRecordsInitial.setMedications("Dolliprane : 500mg");
-        medicalRecordsInitial.setAllergies("aspirine");
+        medicalRecordsInitial.setMedications(arrayMedications);
+        medicalRecordsInitial.setAllergies(arrayAllergies);
         MedicalRecords medicalRecordsUpdated = new MedicalRecords();
         medicalRecordsUpdated.setId(id);
-        medicalRecordsUpdated.setFirstname("Pierre");
-        medicalRecordsUpdated.setLastname("Dupont");
+        medicalRecordsUpdated.setFirstName("Pierre");
+        medicalRecordsUpdated.setLastName("Dupont");
         medicalRecordsUpdated.setBirthdate("01/01/1981");
-        medicalRecordsUpdated.setMedications("Dolliprane : 1000mg");
-        medicalRecordsUpdated.setAllergies("aspirine");
-        when(service.getMedicalRecord(id)).thenReturn(Optional.of(medicalRecordsInitial));
-        when(service.saveMedicalRecord(any(MedicalRecords.class))).thenReturn(medicalRecordsUpdated);
+        medicalRecordsUpdated.setMedications(newArrayMedications);
+        medicalRecordsUpdated.setAllergies(arrayAllergies);
+        when(service.getMedicalRecord(id)).thenReturn(medicalRecordsInitial);
         mockMvc.perform(put("/medicalRecord/{id}", id)
                         .content(asJsonString(medicalRecordsUpdated))
                         .contentType(MediaType.APPLICATION_JSON)
@@ -146,7 +147,8 @@ public class MedicalRecordsControllerTest {
     @Test
     public void testPutOneMedicalRecordsBadRequest() throws Exception {
         Long id = 25L;
-        when(service.getMedicalRecord(id)).thenReturn(Optional.empty());
+        MedicalRecords medicalRecords = new MedicalRecords();
+        when(service.getMedicalRecord(id)).thenReturn(medicalRecords);
         mockMvc.perform(put("/medicalRecord/{id}", id)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
