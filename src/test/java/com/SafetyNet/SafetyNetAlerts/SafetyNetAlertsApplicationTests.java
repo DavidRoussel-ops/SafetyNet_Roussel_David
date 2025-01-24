f44b201ccd25555ba98599c860f54ac6f4254a8f
David <davidroussel171113@outlook.fr>
2025-01-24 17:10:07 +0100

    Retrait de l'appel de BusinessService

--- src/test/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplicationTests.java
+++ src/test/java/com/SafetyNet/SafetyNetAlerts/SafetyNetAlertsApplicationTests.java
@@ -1,17 +1,10 @@
 package com.SafetyNet.SafetyNetAlerts;
 
-import com.SafetyNet.SafetyNetAlerts.Model.Persons;
-import com.SafetyNet.SafetyNetAlerts.Service.BusinessService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SafetyNetAlertsApplicationTests {
-
-	@Autowired
-	BusinessService businessService;
 
 	@Test
 	void contextLoads() {
