8340a693ccd41a9ef91117f9e1aae017adbf71da
David <davidroussel171113@outlook.fr>
2025-01-26 11:21:15 +0100

    Clean code

--- src/test/java/com/SafetyNet/SafetyNetAlerts/Repository/PersonsRepositoryTest.java
+++ src/test/java/com/SafetyNet/SafetyNetAlerts/Repository/PersonsRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.SafetyNet.SafetyNetAlerts.Repository;
 
 import com.SafetyNet.SafetyNetAlerts.Model.Persons;
-import org.junit.After;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
