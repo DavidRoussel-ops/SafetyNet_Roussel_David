b98a55190ccc5589bc524a8cc1f0ded6def6c0ed
David <davidroussel171113@outlook.fr>
2025-01-26 11:14:04 +0100

    Clean code

--- src/main/java/com/SafetyNet/SafetyNetAlerts/ModelDTO/InfolastNameDTO.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/ModelDTO/InfolastNameDTO.java
@@ -1,9 +1,5 @@
 package com.SafetyNet.SafetyNetAlerts.ModelDTO;
 
-
-import lombok.Data;
-
-@Data
 public class InfolastNameDTO {
 
     private String lastname;
@@ -18,6 +14,53 @@ public class InfolastNameDTO {
 
     private String allergies;
 
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMedications() {
+        return medications;
+    }
+
+    public void setMedications(String medications) {
+        this.medications = medications;
+    }
+
+    public String getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
 }
 
 
