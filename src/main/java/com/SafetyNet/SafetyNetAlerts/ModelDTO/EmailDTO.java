97f597dfc94c414d22e75e513e218153329310a7
David <davidroussel171113@outlook.fr>
2025-01-26 11:13:24 +0100

    Clean code

--- src/main/java/com/SafetyNet/SafetyNetAlerts/ModelDTO/EmailDTO.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/ModelDTO/EmailDTO.java
@@ -1,9 +1,14 @@
 package com.SafetyNet.SafetyNetAlerts.ModelDTO;
 
-import lombok.Data;
-
-@Data
 public class EmailDTO {
 
     private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
