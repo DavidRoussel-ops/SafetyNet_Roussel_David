56aca32eb263a7379d4cb2a12b4d14d45803aa36
David <davidroussel171113@outlook.fr>
2025-01-26 11:13:40 +0100

    Clean code

--- src/main/java/com/SafetyNet/SafetyNetAlerts/ModelDTO/FireAddressDTO.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/ModelDTO/FireAddressDTO.java
@@ -1,8 +1,5 @@
 package com.SafetyNet.SafetyNetAlerts.ModelDTO;
 
-import lombok.Data;
-
-@Data
 public class FireAddressDTO {
 
     private String lastname;
@@ -16,4 +13,52 @@ public class FireAddressDTO {
     private String allergies;
 
     private String stations;
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMedications() {
+        return medications;
+    }
+
+    public void setMedications(String medications) {
+        this.medications = medications;
+    }
+
+    public String getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
+
+    public String getStations() {
+        return stations;
+    }
+
+    public void setStations(String stations) {
+        this.stations = stations;
+    }
 }
