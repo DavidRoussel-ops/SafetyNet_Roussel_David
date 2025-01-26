23d0c91238e2f2ed77125e7cb472a4ebeb44744e
David <davidroussel171113@outlook.fr>
2025-01-26 11:12:01 +0100

    Clean code

--- src/main/java/com/SafetyNet/SafetyNetAlerts/Controller/MedicalRecordsController.java
+++ src/main/java/com/SafetyNet/SafetyNetAlerts/Controller/MedicalRecordsController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Optional;
 
 @RestController
 public class MedicalRecordsController {
